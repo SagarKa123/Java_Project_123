24969e35fbaf4274bc8e3835b4f6ed4409adede2
10848708 <Sagar.Kawade2@ltimindtree.com>
2026-01-08 15:13:59 +0530

    Factorial Number File Added

--- src/Factorial_Number.java
+++ src/Factorial_Number.java
@@ -1,6 +1,6 @@
 public class Factorial_Number {
     public static void main(String[] args) {
-        int number = 5; // You can change this value to compute factorial of a different number
+        int number = 10; // You can change this value to compute factorial of a different number
         long factorial = 1;
         for (int i = 1; i <= number; i++) {
             factorial *= i;
